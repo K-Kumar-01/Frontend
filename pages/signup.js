b5965febbea96adf5bd4172e7586775c73d2306b
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2020-09-06 13:14:28 +0530

    added comment for now

--- pages/signup.js
+++ pages/signup.js
@@ -3,29 +3,29 @@ import Head from 'next/head';
 
 import SignupComponent from '../components/auth/SignupComponent';
 import Layout from '../components/Layout';
-import signupImage from '../components/auth/images';
+
 
 const Signup = () => {
 	// for seo friendly sites
-	const head = () => {
-		return (
-			<Head>
-				<title>Signup | TITAN READ</title>
-				<meta name="description" content={`Make your account on TITAN READ`} />
-				<link rel="canonical" href={`/signup`} />
-				<meta property="og:title" content={`SIGNUP | TITAN READ`} />
-				<meta property="og:description" content={`Create account TITAN READ`} />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content={`/signup`} />
-				<meta property="og:site_name" content={`TITAN READ`} />
+	// const head = () => {
+	// 	return (
+	// 		<Head>
+	// 			<title>Signup | TITAN READ</title>
+	// 			<meta name="description" content={`Make your account on TITAN READ`} />
+	// 			<link rel="canonical" href={`/signup`} />
+	// 			<meta property="og:title" content={`SIGNUP | TITAN READ`} />
+	// 			<meta property="og:description" content={`Create account TITAN READ`} />
+	// 			<meta property="og:type" content="website" />
+	// 			<meta property="og:url" content={`/signup`} />
+	// 			<meta property="og:site_name" content={`TITAN READ`} />
 
-				<meta property="og:image" content={`${signupImage}`} />
-				<meta property="og:image:secure_url" content={`${signupImage}`} />
-				<meta property="og:image:type" content="image/jpeg" />
-				{/* <meta property="fb:app_id" content={FB_APP_ID} /> */}
-			</Head>
-		);
-	};
+	// 			<meta property="og:image" content={`${signupImage}`} />
+	// 			<meta property="og:image:secure_url" content={`${signupImage}`} />
+	// 			<meta property="og:image:type" content="image/jpeg" />
+	// 			{/* <meta property="fb:app_id" content={FB_APP_ID} /> */}
+	// 		</Head>
+	// 	);
+	// };
 
 	return (
 		<Layout>
