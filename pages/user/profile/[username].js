06e1129f678cf843fc607cdeca673cb7cf6c06be
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2020-09-15 00:48:25 +0530

    fetch profile info on page load

--- pages/user/profile/[username].js
+++ pages/user/profile/[username].js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Error from 'next/error';
+
 import UserProfile from '../../../components/user/UserProfile';
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
 import Layout from '../../../components/Layout';
+import { getUserDetails } from '../../../actions/user';
 
-
-const UserIndex = () => {
+const UserIndex = (props) => {
+	useEffect(() => {
+		console.log(props);
+	}, []);
 	return (
-		<Layout>
-			<Header></Header>
-			<UserProfile />
-			<Footer></Footer>
-		</Layout>
+		<React.Fragment>
+			{props.error ? (
+				<Error statusCode={props.error} />
+			) : (
+				<Layout>
+					<Header></Header>
+					<UserProfile />
+					<Footer></Footer>
+				</Layout>
+			)}
+		</React.Fragment>
 	);
+};
+
+UserIndex.getInitialProps = async (props) => {
+	let response;
+
+	try {
+		response = await getUserDetails(props.query.username);
+	} catch (error) {
+		console.log(error);
+		return { error: 500 };
+	}
+
+	if (response.error) {
+		return { error: response.error.status };
+	} else {
+		return { userDetails: response.response.data };
+	}
 };
 
 export default UserIndex;
