55ccc5aaf346ac05b08ef870f57e8948615c4c10
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-18 16:28:31 +0530

    Added token in create article header
    Token contains user details
    Used to define who is creating the article

--- actions/article.js
+++ actions/article.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
-import { ARTICLE_CREATE } from '../appConstants';
+import axios from "axios";
+import { ARTICLE_CREATE, COOKIE_NAME } from "../appConstants";
+import { getCookie } from "../helpers/auth";
 
 export const createArticle = async (data) => {
-	let response;
-	try {
-		response = await axios.post(`${ARTICLE_CREATE}`, data, {
-			headers: { 'Content-Type': 'multipart/form-data' },
-		});
-	} catch (error) {
-		throw new Error(error.response.data.message || error.message);
-	}
+  let token;
+  token = getCookie(COOKIE_NAME);
+  let response;
+  try {
+    response = await axios.post(`${ARTICLE_CREATE}`, data, {
+      headers: { "Content-Type": "multipart/form-data", Authorization: token },
+    });
+  } catch (error) {
+    throw new Error(error.response.data.message || error.message);
+  }
 
-	return response;
+  return response;
 };
