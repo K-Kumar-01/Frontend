ff4eb1081a6d4fcd82ce0b0eaaf16a2389f7b263
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-20 16:09:52 +0530

    added sidebar basics

--- components/Header.js
+++ components/Header.js
@@ -1,106 +1,190 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { IconContext } from 'react-icons';
-import { RiLogoutBoxRLine } from 'react-icons/ri';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { IconContext } from "react-icons";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { FcAndroidOs, FcHome } from "react-icons/fc";
 
-import { authenticate, removeCookie } from '../helpers/auth';
-import { COOKIE_NAME } from '../appConstants';
-import { logoutUser } from '../actions/auth';
-import { useRouter } from 'next/router';
-import styles from './Header.module.css';
+import { authenticate, removeCookie } from "../helpers/auth";
+import { COOKIE_NAME } from "../appConstants";
+import { logoutUser } from "../actions/auth";
+import { useRouter } from "next/router";
+import styles from "./Header.module.css";
 
-const Header = () => {
-	const [loggedIn, setLoggedIn] = useState(false);
-	useEffect(() => {
-		setLoggedIn(authenticate(COOKIE_NAME));
-	}, []);
-	const router = useRouter();
+const Header = (props) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [drawerPos, setDrawerPos] = useState(0);
+  const [drawerClass, setDrawerClass] = useState([]);
+  const [mainClass, setMainClass] = useState([]);
 
-	const logout = async () => {
-		let response;
-		try {
-			response = await logoutUser();
-			if (response.error) {
-				console.log(error);
-			} else {
-				removeCookie(COOKIE_NAME);
-				router.push('/signin');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  useEffect(() => {
+    setLoggedIn(authenticate(COOKIE_NAME));
+    console.log(props);
+  }, []);
 
-	return (
-		<nav className="navbar navbar-expand-lg navbar-top navbar-light bg-light sticky-top">
-			<div className="container">
-				<Link href="/">
-					<a className="navbar-brand">Titan Read</a>
-				</Link>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
+  useEffect(() => {
+    if (drawerPos === 1) {
+      setDrawerClass([...drawerClass, "drawerMin"]);
+      setMainClass([...mainClass, "mainMin"]);
+    } else if (drawerPos === 2) {
+      setDrawerClass([...drawerClass, "drawerOpen"]);
+      setMainClass([...mainClass, "mainOpen"]);
+    } else {
+      setDrawerClass([]);
+      setMainClass([]);
+    }
+  }, [drawerPos]);
 
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav ml-auto d-flex align-items-center">
-						<li className="nav-item">
-							<Link href="/membership">
-								<button className={`${styles.link} btn`}>Subscribe</button>
-							</Link>
-						</li>
+  const router = useRouter();
 
-						{loggedIn ? (
-							<li className="nav-item dropdown">
-								<span
-									className="nav-link dropdown-toggle"
-									id="navbarDropdown"
-									role="button"
-									data-toggle="dropdown"
-									aria-haspopup="true"
-									aria-expanded="false"
-								>
-									<img src={loggedIn.avatar} className={`${styles.avatar}`} />
-								</span>
-								<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-									<Link href={`/user/edit/${loggedIn.username}`}>
-										<a className="dropdown-item">Profile</a>
-									</Link>
-									<Link href={`/user/profile/${loggedIn.username}`}>
-										<a className="dropdown-item">Dashboard</a>
-									</Link>
-									<a className="dropdown-item" href="#">
-										My Posts
-									</a>
-									<div className="dropdown-divider"></div>
+  const handleDrawer = () => {
+    setDrawerPos((drawerPos + 1) % 3);
+  };
 
-									<button className="dropdown-item" onClick={logout}>
-										<IconContext.Provider value={{ size: '1.1rem' }}>
-											<RiLogoutBoxRLine />
-										</IconContext.Provider>
-										{` `}Logout
-									</button>
-								</div>
-							</li>
-						) : (
-							<li className="nav-item">
-								<Link href="/signup">
-									<button className={`btn btn-outline-success`}>Get Started</button>
-								</Link>
-							</li>
-						)}
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+  const logout = async () => {
+    let response;
+    try {
+      response = await logoutUser();
+      if (response.error) {
+        console.log(error);
+      } else {
+        removeCookie(COOKIE_NAME);
+        router.push("/signin");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const makeClassNameString = (data) => {
+    let w = "";
+    data.forEach((d) => {
+      let z = styles[`${d}`];
+      console.log(z);
+      w += `${z} `;
+    });
+    return w;
+  };
+
+  return (
+    <div className={`${styles.App}`}>
+      <nav className="navbar navbar-expand-lg navbar-top navbar-light bg-light sticky-top">
+        <div className="container">
+          {props.sidebar ? (
+            <i className="material-icons" onClick={handleDrawer}>
+              menu
+            </i>
+          ) : (
+            <Link href="/">
+              <a className="navbar-brand">Titan Read</a>
+            </Link>
+          )}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto d-flex align-items-center">
+              <li className="nav-item">
+                <Link href="/membership">
+                  <button className={`${styles.link} btn`}>Subscribe</button>
+                </Link>
+              </li>
+
+              {loggedIn ? (
+                <li className="nav-item dropdown">
+                  <span
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <img src={loggedIn.avatar} className={`${styles.avatar}`} />
+                  </span>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <Link href={`/user/edit/${loggedIn.username}`}>
+                      <a className="dropdown-item">Profile</a>
+                    </Link>
+                    <Link href={`/user/profile/${loggedIn.username}`}>
+                      <a className="dropdown-item">Dashboard</a>
+                    </Link>
+                    <a className="dropdown-item" href="#">
+                      My Posts
+                    </a>
+                    <div className="dropdown-divider"></div>
+
+                    <button className="dropdown-item" onClick={logout}>
+                      <IconContext.Provider value={{ size: "1.1rem" }}>
+                        <RiLogoutBoxRLine />
+                      </IconContext.Provider>
+                      {` `}Logout
+                    </button>
+                  </div>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <Link href="/signup">
+                    <button className={`btn btn-outline-success`}>
+                      Get Started
+                    </button>
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {props.sidebar && (
+        <>
+          <aside className={makeClassNameString(drawerClass)}>
+            <ul>
+              <li>
+                <i>
+                  <IconContext.Provider value={{ size: "2rem" }}>
+                    <FcHome />
+                  </IconContext.Provider>
+                </i>
+                <Link href="/">
+                  <a className={`navbar-brand ${styles.link}`}>Titan Read</a>
+                </Link>
+              </li>
+              <li>
+                <i>
+                  <IconContext.Provider value={{ size: "2rem" }}>
+                    <FcAndroidOs />
+                  </IconContext.Provider>
+                </i>
+                <span>Technology</span>
+              </li>
+              <li>
+                <i className="material-icons">show_chart</i>
+                <span>Sales</span>
+              </li>
+              <li>
+                <i className="material-icons">table_chart</i>
+                <span>Others</span>
+              </li>
+            </ul>
+          </aside>
+          <main className={makeClassNameString(mainClass)}>
+            {props.children}
+          </main>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Header;
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,50 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import { getCategories } from '../../actions/category';
+import React, { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { getCategories } from "../../actions/category";
 
-import styles from './index.module.css';
-import Layout from '../../components/Layout';
-import Loading from '../../components/spinner/Loading';
+import styles from "./index.module.css";
+import Layout from "../../components/Layout";
+import Loading from "../../components/spinner/Loading";
 
 const Articles = () => {
-	const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-	useEffect(() => {
-		getCats();
-	}, []);
+  useEffect(() => {
+    getCats();
+  }, []);
 
-	const getCats = async () => {
-		try {
-			// console.log('mew here');
-			const result = await getCategories();
-			setCategories(result.categories);
-			console.log(result);
-			// console.log('mre there');
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const getCats = async () => {
+    try {
+      // console.log('mew here');
+      const result = await getCategories();
+      setCategories(result.categories);
+      console.log(result);
+      // console.log('mre there');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const renderCategories = () => {
-		return categories.map((c) => {
-			return (
-				<div key={c._id} className={`mr-3 ${styles.categories}`} style={{ cursor: 'pointer' }}>
-					{c.name}
-				</div>
-			);
-		});
-	};
+  const renderCategories = () => {
+    return categories.map((c) => {
+      return (
+        <div
+          key={c._id}
+          className={`mr-3 ${styles.categories}`}
+          style={{ cursor: "pointer" }}
+        >
+          {c.name}
+        </div>
+      );
+    });
+  };
 
-	return (
-		<Layout>
-			<Header />
-			<div className={`mx-auto my-3 py-3 px-3 d-flex`} style={{ width: '90%', overflowX: 'auto' }}>
-				{categories.length === 0 ? <Loading /> : renderCategories()}
-			</div>
-			<Footer />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Header sidebar>
+        <div
+          className={`mx-auto my-3 py-3 px-3 d-flex`}
+          style={{ width: "90%", overflowX: "auto" }}
+        >
+          {categories.length === 0 ? <Loading /> : renderCategories()}
+        </div>
+      </Header>
+      <Footer />
+    </Layout>
+  );
 };
 
 export default Articles;
