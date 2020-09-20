526301583c3fa858cf9bf6273cd059a0938a0939
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-21 00:51:21 +0530

    Removed fetch categories
    Categories not required now
    to fetch categories removed function

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,55 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { getCategories } from "../../actions/category";
 
-import styles from "./index.module.css";
 import Layout from "../../components/Layout";
-import Loading from "../../components/spinner/Loading";
+
 
 const Articles = () => {
   const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    getCats();
-  }, []);
-
-  const getCats = async () => {
-    try {
-      // console.log('mew here');
-      const result = await getCategories();
-      setCategories(result.categories);
-      console.log(result);
-      // console.log('mre there');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const renderCategories = () => {
-    return categories.map((c) => {
-      return (
-        <div
-          key={c._id}
-          className={`mr-3 ${styles.categories}`}
-          style={{ cursor: "pointer" }}
-        >
-          {c.name}
-        </div>
-      );
-    });
-  };
+  useEffect(() => {}, []);
 
   return (
     <Layout>
-      <Header sidebar>
-        <div
-          className={`mx-auto my-3 py-3 px-3 d-flex`}
-          style={{ width: "90%", overflowX: "auto" }}
-        >
-          {categories.length === 0 ? <Loading /> : renderCategories()}
-        </div>
-      </Header>
+      <Header sidebar></Header>
       <Footer />
     </Layout>
   );
