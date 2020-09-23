6ffcc4295aa86bf8b75506f662a397d92f30e05d
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-23 20:20:50 +0530

    Edited Article List
    Main article display added

--- components/articles/ArticleList.js
+++ components/articles/ArticleList.js
@@ -5,16 +5,37 @@ const ArticleList = (props) => {
 
   const renderArticles = (data) => {};
 
+  const renderMainArticle = (index = 0) => {
+    return (
+      <div className={`col-12`}>
+        <div className={`d-flex`}>
+          <div className={`w-50`}>
+            <img
+              className={`img-fluid`}
+              src={`${articles[index].featuredPhoto}`}
+              title={articles[index].title}
+              alt={articles[index].title}
+              style={{ maxHeight: "360px" }}
+            />
+          </div>
+          <div className={`w-50`}>
+            <h3>{articles[index].title}</h3>
+            {articles[index].mdesc + `...`}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={`container`}>
       <section className={`trending`}>
         <h1>TRENDING</h1>
       </section>
+      <hr />
       <section className={`latest`}>
         <h1>LATEST</h1>
-        <div className={`row`}>
-          <div className={`col-12`}></div>
-        </div>
+        <div className={`row`}>{renderMainArticle(0)}</div>
       </section>
     </div>
   );
