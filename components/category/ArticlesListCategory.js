5e55bef50537e5be85d7609c7d93c87a407156f8
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-26 17:07:06 +0530

    Added code when article length is 0
    will display an emoji
    will show no article created

--- components/category/ArticlesListCategory.js
+++ components/category/ArticlesListCategory.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import { HiOutlineEmojiSad } from "react-icons/hi";
+
 import ArticleCategoryCard from "./ArticleCategoryCard";
 
 const ArticlesListCategory = (props) => {
   const renderArticles = (data) => {
     if (!data.length) {
-      return <div>No articles</div>;
+      return (
+        <section className={`text-center`}>
+          <IconContext.Provider value={{ color: `#FED15A`, size: "4rem" }}>
+            <div>
+              <HiOutlineEmojiSad />
+            </div>
+          </IconContext.Provider>
+          <p>No Articles of this category have been created</p>
+        </section>
+      );
     } else {
       return (
         <section>
@@ -16,7 +28,9 @@ const ArticlesListCategory = (props) => {
     }
   };
 
-  return <div>{renderArticles(props.articles)}</div>;
+  return (
+    <div style={{ minHeight: "65vh" }}>{renderArticles(props.articles)}</div>
+  );
 };
 
 export default ArticlesListCategory;
