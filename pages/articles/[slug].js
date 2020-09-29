e5cb2f5f10f7315a9881acbdeb224ceb8da8ecfd
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-29 13:45:36 +0530

    Added basic layout for the single article
    Added the page to view single article

---
+++ pages/articles/[slug].js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const SingleArticle = () => {
+  return (
+    <Layout>
+      <Header sidebar></Header>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default SingleArticle;
