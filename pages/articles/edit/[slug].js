22eda9f7b3b5194b05abd306f139e323999b2cc4
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-03 00:32:23 +0530

    Added the
    Error code for page if not found
    redirect if not auth
    response if neither of above

--- pages/articles/edit/[slug].js
+++ pages/articles/edit/[slug].js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ToastProvider, useToasts } from "react-toast-notifications";
+import Error from "next/error";
 
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
@@ -8,8 +9,9 @@ import { authenticate } from "../../../helpers/auth";
 import { COOKIE_NAME } from "../../../appConstants";
 import { useRouter } from "next/router";
 import Preloader from "../../../components/spinner/Preloader";
+import { getParticularArticle } from "../../../actions/article";
 
-const ComponentWithToasts = () => {
+const ComponentWithToasts = (props) => {
   const { addToast } = useToasts();
   const [isAuth, setIsAuth] = useState(false);
   const router = useRouter();
@@ -24,29 +26,52 @@ const ComponentWithToasts = () => {
       router.push("/signin");
     }
   }, []);
+
+  const renderPageContents = () =>
+    isAuth ? (
+      <main>
+        <Layout>
+          <Header />
+
+          <Footer />
+        </Layout>
+      </main>
+    ) : (
+      <Preloader message={`Redirecting to signin page`} />
+    );
+
+  return <>{renderPageContents()}</>;
+};
+
+const EditArticlePage = (props) => {
+
   return (
     <>
-      {isAuth ? (
-        <main>
-          <Layout>
-            <Header />
-
-            <Footer />
-          </Layout>
-        </main>
+      {props.error ? (
+        <Error statusCode={props.error} />
       ) : (
-        <Preloader message={`Redirecting to signin page`} />
+        <ToastProvider placement="bottom-right">
+          <ComponentWithToasts />
+        </ToastProvider>
       )}
     </>
   );
 };
 
-const EditArticlePage = () => {
-  return (
-    <ToastProvider>
-      <ComponentWithToasts />
-    </ToastProvider>
-  );
+EditArticlePage.getInitialProps = async (props) => {
+  let response;
+  console.log(props.query.slug);
+  try {
+    response = await getParticularArticle(props.query.slug);
+  } catch (error) {
+    return { error: 500 };
+  }
+  console.log(response);
+  if (response.error) {
+    return { error: response.error.status };
+  } else {
+    return { article: response.article, articles: response.articles };
+  }
 };
 
 export default EditArticlePage;
