9b49d8c7357a7fa783ebb6469dd1f8b8aa5f2c1e
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-06 21:22:08 +0530

    Added the state and
    Rendered the edit icon conditionally

--- components/help/SingleHelp.js
+++ components/help/SingleHelp.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ToastProvider, useToasts } from "react-toast-notifications";
 import Link from "next/link";
 
-import styles from "./SingleHelp.module.css"
+import { IconContext } from "react-icons";
+import { FaEdit } from "react-icons/fa";
+
+import styles from "./SingleHelp.module.css";
+import { authenticate } from "../../helpers/auth";
+import { COOKIE_NAME } from "../../appConstants";
 
 const ToastedComponentSingleHelp = (props) => {
   const { addToast } = useToasts();
   const { request } = props;
+
+  const [tokenDetails, setTokenDetails] = useState(false);
+
+  useEffect(() => {
+    let tokenData = authenticate(COOKIE_NAME);
+    setTokenDetails(tokenData);
+  }, []);
+
+  const giveStatus = (data) => {
+    switch (data) {
+      case "OPEN":
+        return "text-success";
+      case "CLOSED":
+        return "text-danger";
+      case "PENDING":
+        return "text-warning";
+      default:
+        return "text-info";
+    }
+  };
+
   return (
     <section style={{ minHeight: "70vh" }}>
       <div className={`container`}>
-        <h2 className={`heading text-capitalize`}>Request Details</h2>
+        <div className={`d-flex justify-content-between`}>
+          <h2 className={`heading text-capitalize`}>Request Details</h2>
+          {tokenDetails.username === request.postedBy.username && (
+            <IconContext.Provider value={{ size: "2rem", color: "#17A2B8" }}>
+              <div title="Edit" style={{ cursor: "pointer" }}>
+                <FaEdit />
+              </div>
+            </IconContext.Provider>
+          )}
+        </div>
+        <hr />
         <div className={`row`}>
           <div className={`col-3`}>
             <h4>
@@ -53,6 +89,18 @@ const ToastedComponentSingleHelp = (props) => {
           </div>
           <div className={`col-9 ${styles.changedFont}`}>
             <p>{new Date(request.createdAt).toDateString()}</p>
+          </div>
+        </div>
+        <div className={`row`}>
+          <div className={`col-3`}>
+            <h4>
+              <strong className={`text-uppercase`}>Status</strong>
+            </h4>
+          </div>
+          <div className={`col-9 ${styles.changedFont}`}>
+            <h4 className={`text-capitalize ${giveStatus(request.status)}`}>
+              {request.status}
+            </h4>
           </div>
         </div>
       </div>
