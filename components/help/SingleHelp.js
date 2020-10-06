d93fa4a329bb916dcb0df8f71f37cc0422c91fa5
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-06 20:56:29 +0530

    Rendered basic details of request
    Created the styles
    rendered the basic details of a request

--- components/help/SingleHelp.js
+++ components/help/SingleHelp.js
@@ -1,9 +1,63 @@
 import React from "react";
 import { ToastProvider, useToasts } from "react-toast-notifications";
+import Link from "next/link";
+
+import styles from "./SingleHelp.module.css"
 
 const ToastedComponentSingleHelp = (props) => {
   const { addToast } = useToasts();
-  return <div>hello</div>;
+  const { request } = props;
+  return (
+    <section style={{ minHeight: "70vh" }}>
+      <div className={`container`}>
+        <h2 className={`heading text-capitalize`}>Request Details</h2>
+        <div className={`row`}>
+          <div className={`col-3`}>
+            <h4>
+              <strong>Title</strong>
+            </h4>
+          </div>
+          <div className={`col-9 ${styles.changedFont}`}>
+            <h4>{request.title}</h4>
+          </div>
+        </div>
+        <div className={`row`}>
+          <div className={`col-3`}>
+            <p>
+              <strong>Description</strong>
+            </p>
+          </div>
+          <div className={`col-9 ${styles.changedFont}`}>
+            <p>{request.desc}</p>
+          </div>
+        </div>
+        <div className={`row`}>
+          <div className={`col-3`}>
+            <p>
+              <strong>Requested By</strong>
+            </p>
+          </div>
+          <div className={`col-9 ${styles.changedFont}`}>
+            <p>
+              <Link href={`/user/profile/${request.postedBy.username}`}>
+                <a className={`${styles.link}`}>{request.postedBy.name}</a>
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className={`row`}>
+          <div className={`col-3`}>
+            <p>
+              <strong>Created</strong>
+            </p>
+          </div>
+          <div className={`col-9 ${styles.changedFont}`}>
+            <p>{new Date(request.createdAt).toDateString()}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 const SingleHelp = (props) => {
