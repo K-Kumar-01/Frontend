c03d3d047ac7480b77d45057e700c19718fb10c3
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-07 11:34:28 +0530

    URendered modal to edit request
    Prefilled with values
    Changed the rendering condition of edit button

--- components/help/SingleHelp.js
+++ components/help/SingleHelp.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ToastProvider, useToasts } from "react-toast-notifications";
 import Link from "next/link";
-
+import { useForm } from "react-hook-form";
 import { IconContext } from "react-icons";
 import { FaEdit } from "react-icons/fa";
 
@@ -19,6 +19,13 @@ const ToastedComponentSingleHelp = (props) => {
     let tokenData = authenticate(COOKIE_NAME);
     setTokenDetails(tokenData);
   }, []);
+  const { register, errors, handleSubmit, formState, reset } = useForm({
+    mode: "onTouched",
+    defaultValues: {
+      title: request.title,
+      description: request.desc,
+    },
+  });
 
   const giveStatus = (data) => {
     switch (data) {
@@ -33,18 +40,174 @@ const ToastedComponentSingleHelp = (props) => {
     }
   };
 
+  const onSubmit = async (data, event) => {
+    event.preventDefault();
+    $("#editRequest").modal("hide");
+    let response;
+    // setLoading(true);
+    // try {
+    //   response = await createRequest({
+    //     title: data.title.trim(),
+    //     desc: data.description.trim(),
+    //   });
+    //   setLoading(false);
+    //   if (response.error) {
+    //     addToast(`${response.error}`, {
+    //       appearance: "error",
+    //       autoDismiss: true,
+    //     });
+    //   } else {
+    //     addToast(`${response.message}`, {
+    //       appearance: "success",
+    //       autoDismiss: true,
+    //     });
+    //     router.reload();
+    //   }
+    // } catch (error) {
+    //   setLoading(false);
+    //   addToast(`${error.message}`, {
+    //     appearance: "error",
+    //     autoDismiss: true,
+    //   });
+    // }
+    resetFormState();
+  };
+
+  const resetFormState = () => {
+    reset(
+      {
+        title: request.title,
+        description: request.desc,
+      },
+      {
+        errors: false,
+        dirtyFields: false,
+        isDirty: false,
+        isSubmitted: false,
+        touched: false,
+        isValid: false,
+        submitCount: false,
+      }
+    );
+  };
+
+  const renderRequestForm = () => (
+    <>
+      <div className={`${styles.formLabelGroup}`}>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="Name"
+          name="title"
+          ref={register({ required: true, pattern: /^[a-zA-Z0-9_]*$/ })}
+          autoComplete="off"
+          style={errors.title && { border: "1px solid red" }}
+        />
+        <label htmlFor="title">Title</label>
+        <p className={`text-danger ${styles.errors}`}>
+          {errors.title?.type === "required" && "Title is required"}
+          {errors.title?.type === "pattern" &&
+            "Title can only conatin characters numbers and underscores"}
+        </p>
+      </div>
+      <div>
+        <label htmlFor="description">
+          <h3 className={`heading`}>Description</h3>
+        </label>
+        <textarea
+          name="description"
+          rows="4"
+          ref={register({ required: true, minLength: 15, maxLength: 100 })}
+          placeholder="Description of the request(What you want?)"
+          className={`form-control`}
+          style={errors.description && { border: "1px solid red" }}
+        ></textarea>
+        <p className={`text-danger ${styles.errors}`}>
+          {errors.description?.type === "required" &&
+            "Need a description to create a request"}
+          {errors.description?.type === "minLength" &&
+            "Must be atleast 15 characters long"}
+          {errors.description?.type === "maxLength" &&
+            "Must be atmost 100 characters long"}
+        </p>
+      </div>
+    </>
+  );
+
+  const renderEditRequestModal = () => (
+    <div>
+      <div
+        className="modal fade"
+        id="editRequest"
+        tabIndex="-1"
+        aria-labelledby="editRequestLabel"
+        aria-hidden="true"
+        data-backdrop="static"
+        data-keyboard="false"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title text-center" id="editRequestLabel">
+                Edit Request
+              </h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={resetFormState}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="modal-body">{renderRequestForm()}</div>
+              <div className="modal-footer">
+                <button
+                  type="submit"
+                  className={`btn btn-lg btn-success text-uppercase font-weight-bold mb-2 ${styles.btnLogin}`}
+                  disabled={Object.keys(errors).length !== 0}
+                >
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                  onClick={resetFormState}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <section style={{ minHeight: "70vh" }}>
       <div className={`container`}>
+        {renderEditRequestModal()}
         <div className={`d-flex justify-content-between`}>
           <h2 className={`heading text-capitalize`}>Request Details</h2>
-          {tokenDetails.username === request.postedBy.username && (
-            <IconContext.Provider value={{ size: "2rem", color: "#17A2B8" }}>
-              <div title="Edit" style={{ cursor: "pointer" }}>
-                <FaEdit />
-              </div>
-            </IconContext.Provider>
-          )}
+          {tokenDetails.username === request.postedBy.username &&
+            new Date().getTime() - new Date(request.createdAt).getTime() <
+              1.728e8 && (
+              <IconContext.Provider value={{ size: "2rem", color: "#17A2B8" }}>
+                <div
+                  title="Edit"
+                  style={{ cursor: "pointer" }}
+                  data-toggle="modal"
+                  data-target="#editRequest"
+                >
+                  <FaEdit />
+                </div>
+              </IconContext.Provider>
+            )}
         </div>
         <hr />
         <div className={`row`}>
