718465d5d092be9714440fe08e0261ab12002f58
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-13 18:17:12 +0530

    Added the 404 erro page
    Render the 404 error page

---
+++ pages/404.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+
+const ErrorPage404 = () => {
+  return (
+    <div className="errorContainer">
+      <nav className="shelf">
+        <Link href={``}>
+          <a className="book home-page">Home page</a>
+        </Link>
+        <Link href={`/articles`}>
+          <a className="book about-us">Articles</a>
+        </Link>
+        <Link href={`/help`}>
+          <a className="book contact">Help</a>
+        </Link>
+        <Link href={`/membership`}>
+          <a className="book faq">Membership</a>
+        </Link>
+
+        <span className="book not-found"></span>
+
+        <span className="door left"></span>
+        <span className="door right"></span>
+      </nav>
+      <h1>Error 404</h1>
+      <p>The page you're looking for can't be found</p>
+    </div>
+  );
+};
+
+export default ErrorPage404;
