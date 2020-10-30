4ab3f54389e8cc05ed1675a4e2b22bac7ed780de
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-31 01:04:38 +0530

    Added the head for particular help

--- pages/help/[slug].js
+++ pages/help/[slug].js
@@ -1,24 +1,51 @@
 import React from "react";
 import Error from "next/error";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 import Layout from "../../components/Layout";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { fetchSingleRequest } from "../../actions/help";
 import SingleHelp from "../../components/help/SingleHelp";
+import { DOMAIN } from "../../appConstants";
 
 const SingleHelpPage = (props) => {
+  const router = useRouter();
+
+  const head = () => (
+    <Head>
+      <title>{`Reuqested Article: ${router.query.slug}`}</title>
+      <meta
+        name="description"
+        content={`Article requested: ${router.query.slug}. Description: ${props.request.desc}`}
+      />
+      <link rel="canonical" href={`${DOMAIN}/help/${router.query.slug}`} />
+      <meta property="og:title" content={`${router.query.slug} | TITAN READ`} />
+      <meta
+        property="og:description"
+        content={`Article requested: ${router.query.slug}. Description: ${props.request.desc}`}
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`${DOMAIN}/help/${router.query.slug}`} />
+      <meta property="og:site_name" content="TITAN READ" />
+    </Head>
+  );
+
   return (
     <div>
       {props.error ? (
         <Error statusCode={props.error} />
       ) : (
-        <Layout>
-          <Header sidebar>
-            <SingleHelp request={props.request} />
-          </Header>
-          <Footer />
-        </Layout>
+        <React.Fragment>
+          {head()}
+          <Layout>
+            <Header sidebar>
+              <SingleHelp request={props.request} />
+            </Header>
+            <Footer />
+          </Layout>
+        </React.Fragment>
       )}
     </div>
   );
