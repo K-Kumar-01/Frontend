6699136a7107d318485e22b49c492cb1e4cf6d67
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-11-13 17:58:24 +0530

    Updated the footer and
    Added the logo

--- components/Footer.js
+++ components/Footer.js
@@ -1,46 +1,51 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './Footer.module.css';
+import React from "react";
+import Link from "next/link";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-	return (
-		<footer className={`${styles.footer}`}>
-			<div className="container">
-				<div className="d-flex justify-content-between">
-					<div>
-						<p>Logo here</p>
-						<div className="d-flex justify-content-between">
-							<div>
-								<Link href="/signup">
-									<a className={`${styles.link}`}>Get Started</a>
-								</Link>
-							</div>
-							<div>
-								<Link href="/membership">
-									<a className={`${styles.link}`}>Subscribe </a>
-								</Link>
-							</div>
-							<div>
-								<Link href="/signin">
-									<a className={`${styles.link}`}>Signin</a>
-								</Link>
-							</div>
-						</div>
-						<p>© 2020 Titan Read</p>
-					</div>
-					<div>
-						<p>Contact Us</p>
-						<p>
-							<a className={`${styles.link}`} href="mailto:">
-								Mail
-							</a>
-						</p>
-						<p>Phone Numbers here</p>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className={`${styles.footer}`}>
+      <div className="container py-5">
+        <div className={`row`}>
+          <div className={`col-12 col-md-4`}>
+            <Link href={`/`}>
+              <a className={`${styles.logoLink}`}>
+                <h2 className={`heading ${styles.logo}`}>
+                  T<span className={`${styles.colored}`}>R</span>
+                </h2>
+              </a>
+            </Link>
+            <p className={`py-3`}>
+              <Link href="/signup">
+                <a className={`${styles.link}`}>Get Started</a>
+              </Link>
+              <Link href={`/signin`}>
+                <a className={`${styles.link}`}>Already a member</a>
+              </Link>
+              <Link href={`/articles`}>
+                <a className={`${styles.link}`}>Articles</a>
+              </Link>
+              <Link href={`/membership`}>
+                <a className={`${styles.link} ${styles.nopipe}`}>Pricing</a>
+              </Link>
+            </p>
+            <p className={`${styles.copyright}`}>Titan Read © 2020</p>
+          </div>
+          <div className={`col-12 col-md-6 col-lg-4 ml-auto`}>
+            <h2 className={`heading ${styles.heading}`}>About Titan Read</h2>
+            <p className={`${styles.about} text-justify`}>
+              Titan Read is where those ideas take shape, take off, and spark
+              powerful information. We’re an open platform where readers come to
+              find insightful and dynamic thinking. Here, expert and
+              undiscovered voices alike dive into the heart of any topic and
+              bring new ideas to the surface. Our purpose is to spread these
+              ideas and deepen understanding of the world.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- pages/_document.js
+++ pages/_document.js
@@ -1,38 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-	render() {
-		return (
-			<Html>
-				<Head />
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/logo.ico" />
+        </Head>
 
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-				<script
-					src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-					integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-					crossOrigin="anonymous"
-				></script>
-				<script
-					src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-					integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-					crossOrigin="anonymous"
-				></script>
-				<script
-					src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-					integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-					crossOrigin="anonymous"
-				></script>
-			</Html>
-		);
-	}
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+          integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+          integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+          crossOrigin="anonymous"
+        ></script>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
